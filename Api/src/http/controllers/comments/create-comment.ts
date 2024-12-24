1fb5fe9409e923172d8e03ac93266652c7ea9afb
Andre <andrealbuquerque2001@gmail.com>
2024-12-24 10:03:25 -0300

    feat: create

---
+++ Api/src/http/controllers/comments/create-comment.ts
@@ -0,0 +1,23 @@
+import { makeCreateCommentService } from "@/service/factories/comment/make-create-comment-service";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function createComment(request: FastifyRequest, reply: FastifyReply) {
+    const createcommentBody = z.object({
+        postId: z.string().min(3).max(40),
+        content: z.string().min(3).max(255),
+    })
+
+    const { postId, content } = createcommentBody.parse(request.body)
+
+    try {
+        const createcommentService = makeCreateCommentService()
+
+        await createcommentService.execute({ postId, content, authorId: request.user.sub })
+
+    } catch (error) {
+        return reply.status(400).send({ message: error })
+    }
+
+    return reply.status(204).send()
+}
