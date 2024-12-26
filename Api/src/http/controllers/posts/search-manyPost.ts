0271ba80731a87dd1ad4665350a231d4d181a605
Andre <andrealbuquerque2001@gmail.com>
2024-12-26 12:03:03 -0300

    feat: create

---
+++ Api/src/http/controllers/posts/search-manyPost.ts
@@ -0,0 +1,24 @@
+
+import { PostMapper } from "@/repositories/mapper/post-mapper";
+import { makeSearchManyPostService } from "@/service/factories/post/make-search-many-post-service";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function searchManyPost(request: FastifyRequest, reply: FastifyReply) {
+    const searchmanypostBody = z.object({
+        q: z.string(),
+        page: z.coerce.number().default(1)
+    })
+
+    const { q, page } = searchmanypostBody.parse(request.params)
+
+    try {
+        const searchManyPostService = makeSearchManyPostService()
+
+        const { post } = await searchManyPostService.execute({ query: q, page })
+
+        return reply.status(200).send(PostMapper.toDomainList(post))
+    } catch (error) {
+        return reply.status(400).send({ message: error })
+    }
+}
