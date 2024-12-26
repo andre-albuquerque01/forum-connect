bdd0a696a7849d47ef56a3b1c305ba427eb9c334
Andre <andrealbuquerque2001@gmail.com>
2024-12-26 12:03:03 -0300

    feat: create

---
+++ Api/src/http/controllers/posts/update-post.spec.ts
@@ -0,0 +1,30 @@
+import { app } from "@/app"
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
+import request from 'supertest'
+import { prisma } from "@/lib/prisma"
+import { makeAuthenticateUserFactory } from "@/test/factories/make-authenticate-user-factory"
+import { MakePostFactory } from "@/test/factories/make-post-factory"
+import { MakeUserFactory } from "@/test/factories/make-user-factory"
+
+describe('Delete Post (E2E)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to update post', async () => {
+        const user = await MakeUserFactory.create({})
+        const post = await MakePostFactory.create({ authorId: user.id })
+        const { token } = await makeAuthenticateUserFactory(app)
+
+        const response = await request(app.server)
+            .get(`/updatePost/${post.id}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send()
+
+        expect(response.status).toBe(204)
+    })
+})
