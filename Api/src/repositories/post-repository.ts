3a88397401c3358b42ade9b2c72324327f400db1
Andre <andrealbuquerque2001@gmail.com>
2024-12-28 09:05:01 -0300

    feat: create

---
+++ Api/src/repositories/post-repository.ts
@@ -0,0 +1,29 @@
+import { Prisma, Post } from "@prisma/client"
+import { PaginationParams } from "../utils/paginationParams"
+
+export interface PostsRepository {
+    findById(id: string): Promise<Post | null>
+    searchMany(query: string, params: PaginationParams): Promise<Prisma.PostGetPayload<{
+        include: {
+            author: true;
+            Comment: {
+                include: {
+                    author: true;
+                };
+            };
+        };
+    }>[]>
+    findManyRecent(params: PaginationParams): Promise<Prisma.PostGetPayload<{
+        include: {
+            author: true;
+            Comment: {
+                include: {
+                    author: true;
+                };
+            };
+        };
+    }>[]>
+    create(data: Prisma.PostUncheckedCreateInput): Promise<Post>
+    save(data: Prisma.PostUpdateInput): Promise<void>
+    delete(data: Post): Promise<void>
+}
