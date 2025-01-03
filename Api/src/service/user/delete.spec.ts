fa8ec06d741d675ee323a496bdd8f7c77c83c4d0
Andre <andrealbuquerque2001@gmail.com>
2025-01-03 10:51:19 -0300

    feat: create

---
+++ Api/src/service/user/delete.spec.ts
@@ -0,0 +1,36 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import { InMemoryUserRepository } from "@/repositories/in-memory/in-memory-user-repository"
+import { DeleteService } from "./delete"
+import { RegisterService } from "./register"
+
+let sut: DeleteService
+let userRepository: InMemoryUserRepository
+
+describe('Delete service user', () => {
+    beforeEach(() => {
+        userRepository = new InMemoryUserRepository()
+        sut = new DeleteService(userRepository)
+    })
+
+    it("should be able delete user", async () => {
+        const user  = await userRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: 'password123',
+            nickname: 'john_doe'
+        })
+
+        const { message } = await sut.execute({
+            id: user.id
+        })
+
+        expect(message).toEqual("User deleted")
+    })
+
+
+    it("should be able not found", async () => {
+        await expect(() => sut.execute({
+            id: '1'
+        })).rejects.toBeInstanceOf(Error)
+    })
+})
