25ad564943e948ad97a03365845d2655d07ef8bf
Andre <andrealbuquerque2001@gmail.com>
2025-01-03 10:51:19 -0300

    feat: create

---
+++ Api/src/service/user/profile.spec.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import { InMemoryUserRepository } from "@/repositories/in-memory/in-memory-user-repository"
+import { ProfileService } from "./profile"
+import { RegisterService } from "./register"
+
+let registerService: RegisterService
+let sut: ProfileService
+let userRepository: InMemoryUserRepository
+
+describe('Profile service user', () => {
+    beforeEach(() => {
+        userRepository = new InMemoryUserRepository()
+        sut = new ProfileService(userRepository)
+        registerService = new RegisterService(userRepository)
+    })
+
+    it("should be able Profile user", async () => {
+        const data = await registerService.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: 'password123',
+            passwordConfirmation: 'password123',
+            nickname: 'john_doe'
+        })
+
+        const { user } = await sut.execute({
+            id: data.user.id
+        })
+
+        expect(user).toEqual(expect.objectContaining({ id: expect.any(String), }))
+    })
+
+
+    it("should not be able access profile", async () => {
+        await expect(() => sut.execute({
+            id: '1'
+        })).rejects.toBeInstanceOf(Error)
+    })
+})
