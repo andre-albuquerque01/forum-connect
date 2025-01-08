e9307fc91de3f452ec89a5662ad9fa955880bc42
andre <andrealbuquerque2001@gmail.com>
2025-01-08 11:24:32 -0300

    feat: update

--- Api/src/http/controllers/posts/routes.ts
+++ Api/src/http/controllers/posts/routes.ts
@@ -6,16 +6,20 @@ import { deletePost } from "./delete-post";
 import { fetchRecentPost } from "./fetch-recent-post";
 import { searchManyPost } from "./search-manyPost";
 import { findByIdPost } from "./find-by-id-post";
+import { searchUserPost } from "./search-user-post";
+import { findByUserPost } from "./find-by-user-post";
 
 export async function postRoutes(app: FastifyInstance) {
     // Public routes
     app.get('/posts/:page', fetchRecentPost)
     app.get('/posts/search/:q/:page', searchManyPost)
+    app.get('/findByIdPost/:id', findByIdPost)
+    app.get('/search-user-post/:id', searchUserPost)
     
-
+    
     // Authenticate
     app.put('/updatePost/:id', { onRequest: [verifyJwt] }, updatePost)
-    app.get('/findByIdPost/:id', { onRequest: [verifyJwt] }, findByIdPost)
+    app.get('/find-user-post', { onRequest: [verifyJwt] }, findByUserPost)
     app.post('/createPost', { onRequest: [verifyJwt] }, createPost)
     app.delete('/deletePost/:id', { onRequest: [verifyJwt] }, deletePost)
 }
