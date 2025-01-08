61d3d60e64d5aa048ae0137dc01fd94dda76117d
andre <andrealbuquerque2001@gmail.com>
2025-01-08 11:24:28 -0300

    feat: update

--- Api/src/http/controllers/posts/delete-post.ts
+++ Api/src/http/controllers/posts/delete-post.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 
 export async function deletePost(request: FastifyRequest, reply: FastifyReply) {
     const deletepostBody = z.object({
-        id: z.string().uuid(),
+        id: z.string(),
     })
 
     const { id } = deletepostBody.parse(request.params)
@@ -14,9 +14,9 @@ export async function deletePost(request: FastifyRequest, reply: FastifyReply) {
 
         await deletepostService.execute({ id, authorId: request.user.sub })
 
+        return reply.status(201).send()
     } catch (error) {
         return reply.status(400).send({ message: error })
     }
 
-    return reply.status(201).send()
 }
