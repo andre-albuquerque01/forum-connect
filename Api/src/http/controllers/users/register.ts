e0eabf2e23bf1035ecbeafcd249d05f10a59e982
Andre <andrealbuquerque2001@gmail.com>
2025-01-09 10:11:08 -0300

    feat: update

--- Api/src/http/controllers/users/register.ts
+++ Api/src/http/controllers/users/register.ts
@@ -7,20 +7,32 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         name: z.string().min(3).max(40),
         nickname: z.string().min(3).max(20),
         email: z.string().email(),
-        password: z.string().min(8),
-        passwordConfirmation: z.string().min(8),
-    })
+        termService: z.boolean(),
+        password: z
+            .string()
+            .min(8, { message: "A senha deve ter pelo menos 8 caracteres." })
+            .regex(/[A-Z]/, { message: "A senha deve conter pelo menos uma letra maiúscula." })
+            .regex(/[0-9]/, { message: "A senha deve conter pelo menos um número." })
+            .regex(/[^A-Za-z0-9]/, { message: "A senha deve conter pelo menos um caractere especial." }),
+        passwordConfirmation: z
+            .string()
+            .min(8, { message: "A confirmação de senha deve ter pelo menos 8 caracteres." }),
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        path: ["passwordConfirmation"],
+        message: "As senhas devem coincidir.",
+    });
 
-    const { name, email, password, passwordConfirmation, nickname } = registerBody.parse(request.body)
+    const { name, email, password, passwordConfirmation, nickname, termService } = registerBody.parse(request.body)
 
     try {
         const registerService = makeRegisterService()
 
-        await registerService.execute({ name, email, password, passwordConfirmation, nickname })
+        await registerService.execute({ name, email, password, passwordConfirmation, nickname, termService })
 
     } catch (error) {
-        return reply.status(400).send({ message: error })
+        return reply.status(400).send({
+            error: error instanceof Error ? error.message : "Erro desconhecido.",
+        });
     }
-
     return reply.status(204).send()
 }
