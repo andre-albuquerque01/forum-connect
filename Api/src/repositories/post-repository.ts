216c12725ac55a4d28ad7c342845202ae8e82a00
Andre <andrealbuquerque2001@gmail.com>
2025-01-09 10:11:08 -0300

    feat: update

--- Api/src/repositories/post-repository.ts
+++ Api/src/repositories/post-repository.ts
@@ -3,6 +3,16 @@ import { PaginationParams } from "../utils/paginationParams"
 
 export interface PostsRepository {
     findById(id: string): Promise<Post | null>
+    findByUserPost(id: string):  Promise<Prisma.PostGetPayload<{
+        include: {
+            author: true;
+            Comment: {
+                include: {
+                    author: true;
+                };
+            };
+        };
+    }>[]>
     searchMany(query: string, params: PaginationParams): Promise<Prisma.PostGetPayload<{
         include: {
             author: true;
