4d1010b2da9beef30c0cd23b6fcedd9d781be42f
Andre <andrealbuquerque2001@gmail.com>
2025-01-09 10:11:08 -0300

    feat: update

--- Api/src/repositories/in-memory/in-memory-recover-password-repository.ts
+++ Api/src/repositories/in-memory/in-memory-recover-password-repository.ts
@@ -4,7 +4,12 @@ import { randomUUID } from "node:crypto";
 
 export class InMemoryRecoverPassword implements RecoverPasswordRepository {
     private items: PasswordRecover[] = [];
-
+    
+    async findByUserId(userId: string): Promise<PasswordRecover | null> {
+        const recover = this.items.find(item => item.userId === userId)
+        return recover ? recover : null
+    }
+    
     async findByToken(token: string): Promise<PasswordRecover | null> {
         const recover = this.items.find(item => item.token === token)
         return recover ? recover : null
@@ -21,6 +26,14 @@ export class InMemoryRecoverPassword implements RecoverPasswordRepository {
         this.items.push(recoverPassowrd)
 
         return recoverPassowrd
+    }
+
+    async save(data: PasswordRecover): Promise<PasswordRecover> {
+        const index = this.items.findIndex(item => item.id === data.id)
+        if (index > 0) {
+            this.items[index] = data
+        }
+        return data
     }
 
     async delete(data: PasswordRecover): Promise<void> {
