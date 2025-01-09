19b9c8d3fd95100feb6a8f44217d32e41bffc513
Andre <andrealbuquerque2001@gmail.com>
2025-01-09 10:11:08 -0300

    feat: update

--- Api/src/repositories/recover-password-repository.ts
+++ Api/src/repositories/recover-password-repository.ts
@@ -1,7 +1,9 @@
 import { PasswordRecover, Prisma } from "@prisma/client"
 
 export interface RecoverPasswordRepository {
+    findByUserId(userId: string): Promise<PasswordRecover | null>
     findByToken(token: string): Promise<PasswordRecover | null>
     create(data: Prisma.PasswordRecoverUncheckedCreateInput): Promise<PasswordRecover>
+    save(data: PasswordRecover): Promise<PasswordRecover>
     delete(data: PasswordRecover): Promise<void>
 }
