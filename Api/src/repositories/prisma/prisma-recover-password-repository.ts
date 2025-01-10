78ac58f8e0cb220787ccb97ca4e1c4798a99a262
Andre <andrealbuquerque2001@gmail.com>
2025-01-10 09:51:58 -0300

    feat: update

--- Api/src/repositories/prisma/prisma-recover-password-repository.ts
+++ Api/src/repositories/prisma/prisma-recover-password-repository.ts
@@ -3,6 +3,17 @@ import { RecoverPasswordRepository } from "../recover-password-repository";
 import { prisma } from "@/lib/prisma";
 
 export class PrismaRecoverPasswordRepository implements RecoverPasswordRepository {
+
+    async findByUserId(userId: string): Promise<PasswordRecover | null> {
+        const passwordRecover = await prisma.passwordRecover.findFirst({
+            where: {
+                userId
+            }
+        })
+        if (!passwordRecover) return null
+        return passwordRecover
+    }
+
     async findByToken(token: string): Promise<PasswordRecover | null> {
         const passwordRecover = await prisma.passwordRecover.findUnique({
             where: {
@@ -12,12 +23,23 @@ export class PrismaRecoverPasswordRepository implements RecoverPasswordRepositor
         if (!passwordRecover) return null
         return passwordRecover
     }
+
     async create(data: Prisma.PasswordRecoverUncheckedCreateInput): Promise<PasswordRecover> {
         const passwordrecover = prisma.passwordRecover.create({
             data
         })
 
         return passwordrecover
+    }
+
+    async save(data: PasswordRecover): Promise<PasswordRecover> {
+        const passwordRecover = await prisma.passwordRecover.update({
+            where: {
+                id: data.id
+            },
+            data,
+        })
+        return passwordRecover
     }
 
     async delete(data: PasswordRecover): Promise<void> {
