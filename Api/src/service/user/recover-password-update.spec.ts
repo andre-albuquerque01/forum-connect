066fffdb9c9407e5fc2ae71fa19130f9b300fe63
Andre <andrealbuquerque2001@gmail.com>
2025-01-11 08:51:58 -0300

    feat: update

--- Api/src/service/user/recover-password-update.spec.ts
+++ Api/src/service/user/recover-password-update.spec.ts
@@ -22,7 +22,8 @@ describe('Recover password send email service', () => {
             name: 'John Doe',
             email: 'johndoe@example.com',
             password: 'password123',
-            nickname: 'john_doe'
+            nickname: 'john_doe',
+            termService: true,
         })
 
         const { token } = await recoverPasswordSendEmail.execute({
@@ -52,7 +53,8 @@ describe('Recover password send email service', () => {
             name: 'John Doe',
             email: 'johndoe@example.com',
             password: 'password123',
-            nickname: 'john_doe'
+            nickname: 'john_doe',
+            termService: true,
         })
 
         const { token } = await recoverPasswordSendEmail.execute({
