1449f346970d4f5880a5eb01c9802ee62c3606ca
Andre <andrealbuquerque2001@gmail.com>
2025-01-14 09:52:18 -0300

    feat: create

---
+++ app/src/app/threads/layout.tsx
@@ -0,0 +1,12 @@
+export default function ThreadsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div suppressHydrationWarning>
+      
+          {children}
+    </div>
+  );
+}
