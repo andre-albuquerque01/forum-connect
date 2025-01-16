4aafaba8d074ef17ce1d5972bb798267ac9a52f6
Andre <andrealbuquerque2001@gmail.com>
2025-01-16 11:11:15 -0300

    feat: update

--- app/src/app/user/login/page.tsx
+++ app/src/app/user/login/page.tsx
@@ -2,6 +2,8 @@ import LoginComponent from "@/components/user/login";
 
 export default function Login() {
     return (
-        <LoginComponent />
+        <div className="bg-forum-gradient-2 h-[calc(100vh-64px)] flex items-center justify-center overflow-hidden p-2">
+            <LoginComponent />
+        </div>
     )
 }
