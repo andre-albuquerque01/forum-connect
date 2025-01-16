b46c7dcd210926fd15565269e6054112c6cbe2ef
Andre <andrealbuquerque2001@gmail.com>
2025-01-16 11:11:15 -0300

    feat: update

--- app/src/app/user/recover-password/send-email/page.tsx
+++ app/src/app/user/recover-password/send-email/page.tsx
@@ -2,6 +2,8 @@ import SendEmailComponent from "@/components/user/recover-password/send-email";
 
 export default function SendEmail() {
     return (
-        <SendEmailComponent />
+        <div className="bg-forum-gradient-2 h-[calc(100vh-64px)] flex items-center justify-center overflow-hidden p-2">
+            <SendEmailComponent />
+        </div>
     )
 }
