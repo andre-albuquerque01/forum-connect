70a691ef5f766411c2b0ab69eba851b55cc193f8
Andre <andrealbuquerque2001@gmail.com>
2025-01-17 10:41:52 -0300

    feat: update

--- app/src/components/user/recover-password/update-password.tsx
+++ app/src/components/user/recover-password/update-password.tsx
@@ -1,18 +1,58 @@
+'use client'
+import { RecoverPasswordUpdate } from "@/app/action";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect } from "react";
+import { useFormStatus } from "react-dom";
 import { FaArrowLeft } from "react-icons/fa";
 
 export default function UpdatePasswordComponent({ token }: { token: string }) {
+    const BtnForm = () => {
+        const { pending } = useFormStatus()
+        return (
+            <>
+                {pending ? (
+                    <button
+                        className="border bg-forum-gradient-1 text-white w-96 py-2 rounded-md max-md:w-80"
+                        disabled={pending}>
+                        Alterando...
+                    </button>
+                ) : (
+                    <button className="border bg-forum-gradient-1 text-white w-96 py-2 rounded-md max-md:w-80">Alterando</button>
+                )}
+            </>
+        )
+    }
+
+    const [state, action] = useActionState(RecoverPasswordUpdate, {
+        ok: false,
+        error: '',
+        data: null,
+    })
+    const router = useRouter()
+
+    useEffect(() => {
+        if (state.ok) {
+            alert('Email de recuperação enviado com sucesso!')
+            router.push('/user/login')
+        }
+    }, [state, router])
+
     return (
         <div className="bg-forum-gradient-2 h-screen w-full flex justify-center items-center">
             <div className="bg-zinc-50 max-w-[1200px] w-[40%] h-[50%] flex flex-col justify-center items-center rounded-md sm:p-2 max-md:w-96 max-sm:mx-2 max-md:h-96 md:w-[560px]">
-                <form className="flex flex-col space-y-2 justify-center items-center h-[220px]">
+                <form action={action} className="flex flex-col space-y-2 justify-center items-center h-[220px]">
                     <Link href="/user/login" className="w-96 max-md:w-80 text-sm flex flex-row items-center text-red-400 hover:text-blue-600">
                         <FaArrowLeft className="w-4 h-4 mr-2 " /> Voltar
                     </Link>
+                    <input type="hidden" name="token" value={token} />
                     <h1 className="w-96 max-md:w-80 font-extrabold bg-forum-gradient-1 bg-clip-text text-transparent text-xl">Alteração do usuário</h1>
                     <input type="password" name="password" placeholder="Senha" className="border p-2 w-96 rounded-md max-md:w-80" />
-                    <input type="password" name="password-confirmation" placeholder="Repetir senha" className="border p-2 w-96 rounded-md max-md:w-80" />
-                    <button type="submit" className="border bg-forum-gradient-1 text-white w-96 py-2 rounded-md max-md:w-80">Cadastrar</button>
+                    <input type="password" name="passwordConfirmation" placeholder="Repetir senha" className="border p-2 w-96 rounded-md max-md:w-80" />
+                    <BtnForm />
+                    <span className="w-96 max-md:w-80 text-justify flex flex-row items-center text-red-600 text-xs">
+                        {state.error && state.error}
+                    </span>
                 </form>
             </div>
         </div>
