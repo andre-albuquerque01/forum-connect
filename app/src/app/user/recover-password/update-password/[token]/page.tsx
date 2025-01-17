514b58f1e8c3cb841550fca612453af7a09014fc
Andre <andrealbuquerque2001@gmail.com>
2025-01-17 10:41:52 -0300

    feat: update

--- app/src/app/user/recover-password/update-password/[token]/page.tsx
+++ app/src/app/user/recover-password/update-password/[token]/page.tsx
@@ -2,7 +2,11 @@ import UpdatePasswordComponent from "@/components/user/recover-password/update-p
 
 export default async function UpdatePassword({ params }: { params: { token: string } }) {
     const { token } = await params
+    const urlToken = decodeURIComponent(token);
+
     return (
-        <UpdatePasswordComponent token={token} />
+        <div className="bg-forum-gradient-2 h-[calc(100vh-64px)] flex items-center justify-center overflow-hidden p-2">
+            <UpdatePasswordComponent token={urlToken} />
+        </div>
     )
 }
