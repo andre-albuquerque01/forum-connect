f7859239158e4dfde2160428e4b55e790a65395f
Andre <andrealbuquerque2001@gmail.com>
2025-01-17 10:41:52 -0300

    feat: update

--- app/src/components/header/navBar.tsx
+++ app/src/components/header/navBar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link"
 import SearchForm from "./searchForm"
-import { BiHome, BiUser } from "react-icons/bi"
-import { LuLogOut } from "react-icons/lu"
+import { BiHome, BiLogIn, BiUser } from "react-icons/bi"
+import { ButtonLogout } from "../user/button-logout"
+import { cookies } from "next/headers"
 
-export const Header = () => {
+export const Header = async () => {
+    const cookistore = await cookies()
+    const token = cookistore.get('token')?.value
     return (
         <nav className="bg-forum-gradient-3 h-16">
             <div className="max-w-7xl mx-auto flex items-center justify-between h-full">
@@ -18,22 +21,27 @@ export const Header = () => {
                     >
                         <BiHome className="w-5 h-5" />
                     </Link>
-                    <Link
-                        href="/user"
-                        className="px-2 py-2 hover:bg-white hover:text-black rounded-md transition"
-                        title="Perfil"
-                    >
-                        <BiUser className="w-5 h-5" />
-                    </Link>
-                    <Link
-                        href="/logout"
-                        className="px-2 py-2 hover:bg-white hover:text-black rounded-md transition"
-                        title="Sair"
-                    >
-                        <LuLogOut className="w-5 h-5" />
-                    </Link>
+                    {token && (
+                        <Link
+                            href="/user"
+                            className="px-2 py-2 hover:bg-white hover:text-black rounded-md transition"
+                            title="Perfil"
+                        >
+                            <BiUser className="w-5 h-5" />
+                        </Link>
+                    )}
+                    {token ? (
+                        <ButtonLogout />
+                    ) : (
+                        <Link
+                            href="/user/login"
+                            className="px-2 py-2 hover:bg-white hover:text-black rounded-md transition"
+                            title="Login"
+                        >
+                            <BiLogIn className="w-5 h-5" />
+                        </Link>
+                    )}
                 </div>
-
             </div>
         </nav>
     )
