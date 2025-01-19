bb364a4bf426f0e74a9e760c7f56e625930764a9
Andre <andrealbuquerque2001@gmail.com>
2025-01-19 19:34:58 -0300

    feat: update

--- app/src/app/user/create-account/page.tsx
+++ app/src/app/user/create-account/page.tsx
@@ -1,9 +1,9 @@
 import CreateAccountComponent from "@/components/user/create-account";
 
-export default function Create() {
+export default function CreateUser() {
     return (
-        <div className="bg-forum-gradient-2 h-[calc(100vh-64px)] flex items-center justify-center overflow-hidden p-2">
+        <section className="bg-forum-gradient-2 h-[calc(100vh-64px)] flex items-center justify-center overflow-hidden p-2">
             <CreateAccountComponent />
-        </div>
+        </section>
     )
 }
