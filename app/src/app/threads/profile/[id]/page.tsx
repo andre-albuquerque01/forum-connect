a2a348b6918602ba020a8b0299a665bc88ecab74
Andre <andrealbuquerque2001@gmail.com>
2025-01-19 19:34:58 -0300

    feat: update

--- app/src/app/threads/profile/[id]/page.tsx
+++ app/src/app/threads/profile/[id]/page.tsx
@@ -1,33 +1,48 @@
-import { GetUserByThreads, SearchUserByThreads } from "@/app/action";
+import { ProfileUser, SearchUserByThreads } from "@/app/action";
 import { CreateCommentComponent } from "@/components/thread/createComment";
 import { ThreadInterface } from "../../(home)/page";
-import { DropdownMenuThreads } from "@/components/header/dropdownMenuThreads";
-import { DropdownMenuComment } from "@/components/header/dropdownMenuComment";
+import { DropdownMenuThreads } from "@/components/nav/dropdownMenuThreads";
+import { DropdownMenuComment } from "@/components/nav/dropdownMenuComment";
+import { cookies } from "next/headers";
+import Link from "next/link";
 
-export default async function Home({ params }: { params: { id: string } }) {
+export default async function Profile({ params }: { params: { id: string } }) {
   const user = await params
-  let questions = [];
 
-  if (user) questions = (await SearchUserByThreads(user.id)) as ThreadInterface[];
-  else questions = (await GetUserByThreads()) as ThreadInterface[];
+  const questions = (await SearchUserByThreads(user.id)) as ThreadInterface[];
+
+  const cookistore = await cookies()
+  const token = cookistore.get('token')?.value
+
+  const profile = await ProfileUser()
 
   return (
-    <div className="bg-forum-gradient-2 w-full min-h-screen flex flex-col items-center py-10">
+    <div className="bg-forum-gradient-2 w-full min-h-[calc(100vh-64px)] flex flex-col items-center py-10">
       {questions.length > 0 && (
         <h1 className="text-4xl font-bold text-white mb-6">
           Forum Questions of {questions[0].author.nickname}
         </h1>
       )}
+      <div className="mb-4">
+        <Link href="/threads">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">
+            Voltar para todas as questões
+          </button>
+        </Link>
+      </div>
       <div className="flex flex-col gap-6 w-full max-w-3xl px-4">
         {questions &&
+          questions.length > 0 ?
           questions.map((q) => (
             <div
               key={q.id}
               className="bg-white p-6 rounded-lg shadow-lg flex flex-col relative"
             >
-              <div className="absolute top-4 right-4">
-                <DropdownMenuThreads questionId={q.id} />
-              </div>
+              {profile && profile.id === q.author.id && (
+                <div className="absolute top-4 right-4">
+                  <DropdownMenuThreads questionId={q.id} />
+                </div>
+              )}
               <h2 className="text-lg font-semibold text-gray-800 mb-2">
                 {q.author.nickname}
               </h2>
@@ -42,20 +57,31 @@ export default async function Home({ params }: { params: { id: string } }) {
                         key={index}
                         className="bg-gray-100 p-2 rounded-md text-gray-700 w-full break-words relative"
                       >
-                        <div className="absolute top-2 right-4">
-                          <DropdownMenuComment commentId={comment.id} />
-                        </div>
+                        {profile && profile.id === comment.author.id && (
+                          <div className="absolute top-2 right-4">
+                            <DropdownMenuComment commentId={comment.id} />
+                          </div>
+                        )}
                         <p>
-                          <strong>{comment.author?.nickname || "Anonymous"}:</strong>{" "}
+                          <Link
+                            href={`/threads/profile/${comment.author.id}`}
+                            className="text-sm font-semibold text-gray-800 mb-2"
+                          >
+                            {comment.author?.nickname || "Anonymous"}: {' '}
+                          </Link>
                           {comment.content}
                         </p>
                       </li>
                     ))}
                 </ul>
               </div>
-              <CreateCommentComponent idQuestion={q.id} />
+              <CreateCommentComponent idQuestion={q.id} token={token ? true : false} />
             </div>
-          ))}
+          )) : (
+            <p className="text-4xl font-bold text-white mb-6">
+              Não há questões publicadas pelo usuário
+            </p>
+          )}
       </div>
     </div>
   );
