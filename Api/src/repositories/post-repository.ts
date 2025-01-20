a5f42eae6a1d1b103be4ff9bd3a5f5fef4c510c1
andre <andrealbuquerque2001@gmail.com>
2025-01-20 10:22:53 -0300

    feat: update

--- Api/src/repositories/post-repository.ts
+++ Api/src/repositories/post-repository.ts
@@ -13,26 +13,32 @@ export interface PostsRepository {
             };
         };
     }>[]>
-    searchMany(query: string, params: PaginationParams): Promise<Prisma.PostGetPayload<{
-        include: {
-            author: true;
-            Comment: {
-                include: {
-                    author: true;
+    searchMany(query: string, params: PaginationParams): Promise<{
+        posts: Prisma.PostGetPayload<{
+            include: {
+                author: true;
+                Comment: {
+                    include: {
+                        author: true;
+                    };
                 };
             };
-        };
-    }>[]>
-    findManyRecent(params: PaginationParams): Promise<Prisma.PostGetPayload<{
-        include: {
-            author: true;
-            Comment: {
-                include: {
-                    author: true;
+        }>[];
+        totalPages: number;
+    }>
+    findManyRecent(params: PaginationParams): Promise<{
+        posts: Prisma.PostGetPayload<{
+            include: {
+                author: true;
+                Comment: {
+                    include: {
+                        author: true;
+                    };
                 };
             };
-        };
-    }>[]>
+        }>[];
+        totalPages: number;
+    }> 
     create(data: Prisma.PostUncheckedCreateInput): Promise<Post>
     save(data: Prisma.PostUpdateInput): Promise<void>
     delete(data: Post): Promise<void>
