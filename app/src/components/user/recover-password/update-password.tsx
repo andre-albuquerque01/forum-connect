f73a5c4eee6303042c68a6f6b2227c0fc42165c6
Andre <andrealbuquerque2001@gmail.com>
2025-01-21 10:44:23 -0300

    feat: update

--- app/src/components/user/recover-password/update-password.tsx
+++ app/src/components/user/recover-password/update-password.tsx
@@ -1,10 +1,9 @@
 'use client'
 import { RecoverPasswordUpdate } from "@/app/action";
-import Link from "next/link";
+import { GoBack } from "@/components/nav/goBack";
 import { useRouter } from "next/navigation";
 import { useActionState, useEffect } from "react";
 import { useFormStatus } from "react-dom";
-import { FaArrowLeft } from "react-icons/fa";
 
 export default function UpdatePasswordComponent({ token }: { token: string }) {
     const BtnForm = () => {
@@ -42,15 +41,13 @@ export default function UpdatePasswordComponent({ token }: { token: string }) {
         <div className="bg-forum-gradient-2 h-screen w-full flex justify-center items-center">
             <div className="bg-zinc-50 max-w-[1200px] w-[40%] h-[50%] flex flex-col justify-center items-center rounded-md sm:p-2 max-md:w-96 max-sm:mx-2 max-md:h-96 md:w-[560px]">
                 <form action={action} className="flex flex-col space-y-2 justify-center items-center h-[220px]">
-                    <Link href="/user/login" className="w-96 max-md:w-80 text-sm flex flex-row items-center text-red-400 hover:text-blue-600">
-                        <FaArrowLeft className="w-4 h-4 mr-2 " /> Voltar
-                    </Link>
+                    <GoBack />
                     <input type="hidden" name="token" value={token} />
                     <h1 className="w-96 max-md:w-80 font-extrabold bg-forum-gradient-1 bg-clip-text text-transparent text-xl">Alteração do usuário</h1>
                     <input type="password" name="password" placeholder="Senha" className="border p-2 w-96 rounded-md max-md:w-80" />
                     <input type="password" name="passwordConfirmation" placeholder="Repetir senha" className="border p-2 w-96 rounded-md max-md:w-80" />
                     <BtnForm />
-                    <span className="w-96 max-md:w-80 text-justify flex flex-row items-center text-red-600 text-xs">
+                    <span className="w-96 max-md:w-80 text-justify flex flex-row items-center text-red-600 text-xs" aria-live="polite">
                         {state.error && state.error}
                     </span>
                 </form>
