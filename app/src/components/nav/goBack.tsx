0f2a51ad47722dad667a922694cc0fe5e5137bf2
Andre <andrealbuquerque2001@gmail.com>
2025-01-23 08:54:52 -0300

    feat: create

---
+++ app/src/components/nav/goBack.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import { FaArrowLeft } from "react-icons/fa"
+
+export const GoBack = () => {
+    const router = useRouter()
+
+    return (
+        <button onClick={() => router.back()} className="w-96 max-md:w-80 text-sm flex flex-row items-center text-red-400 hover:text-blue-600">
+            <FaArrowLeft className="w-4 h-4 mr-2 " /> Voltar
+        </button>
+    )
+}
