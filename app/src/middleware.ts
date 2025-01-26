c2cf1801e77a51fd720eeffd28fc547aa08bf64e
Andre <andrealbuquerque2001@gmail.com>
2025-01-26 08:05:29 -0300

    feat: update

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -4,12 +4,11 @@ import verifyToken from './data/verify-token'
 export async function middleware(request: NextRequest) {
     const token = request.cookies.get('token')?.value
     const authentication = token ? await verifyToken(token) : false
-
+    
     if (
-        (!authentication && (request.nextUrl.pathname.endsWith('user/update') ||
-            request.nextUrl.pathname.startsWith('threads/new') ||
-            request.nextUrl.pathname.endsWith('threads/new') ||
-            request.nextUrl.pathname.startsWith('threads/get/') ||
+        (!authentication && (request.nextUrl.pathname.endsWith('/user/update') ||
+            request.nextUrl.pathname.startsWith('/threads/new') ||
+            request.nextUrl.pathname.startsWith('/threads/get/') ||
             request.nextUrl.pathname.endsWith('user')
         ))
     ) {
@@ -17,9 +16,9 @@ export async function middleware(request: NextRequest) {
     }
 
     if (
-        authentication && (request.nextUrl.pathname.startsWith('user/login') ||
-            request.nextUrl.pathname.startsWith('user/create-account') ||
-            request.nextUrl.pathname.startsWith('user/recover-password')
+        authentication && (request.nextUrl.pathname.startsWith('/user/login') ||
+            request.nextUrl.pathname.startsWith('/user/create-account') ||
+            request.nextUrl.pathname.startsWith('/user/recover-password')
         )
     ) {
         return NextResponse.redirect(new URL('/threads', request.url))
