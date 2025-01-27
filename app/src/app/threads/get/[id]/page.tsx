9e78b063086d45654c2527b8f0b37f1133f4eacf
Andre <andrealbuquerque2001@gmail.com>
2025-01-27 10:53:28 -0300

    feat: update

--- app/src/app/threads/get/[id]/page.tsx
+++ app/src/app/threads/get/[id]/page.tsx
@@ -1,13 +1,14 @@
 import { GetByIdThread } from "@/app/action";
 import UpdateThreadComponent from "@/components/thread/update-thread";
 
-
-export default async function ThreadUpdate({ params }: { params: { id: string } }) {
-  const { id } = await params;
+type Params = Promise<{ id: string }>
+export default async function ThreadUpdate(props: { params: Params }) {
+  const params = await props.params
+  const id = await params.id;
   const { title, content } = await GetByIdThread(id);
 
   return (
-    <div className="bg-forum-gradient-2 w-full min-h-screen flex flex-col items-center py-10">
+    <div className="bg-forum-gradient-2 w-full h-[calc(100vh-64px)] flex flex-col items-center py-10">
       <UpdateThreadComponent id={id} title={title} content={content} />
     </div>
   );
